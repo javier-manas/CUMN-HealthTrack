09208872ad02e553814033d03398b334b9f01e57
javier-manas <javier.manasb@alumnos.upm.es>
2023-04-24 07:42:29 +0200

    se agrega funccionalidad a imc y se inicia desarrollo de recompensas

--- app/src/main/java/com/example/healthtrack/ImcActivity.kt
+++ app/src/main/java/com/example/healthtrack/ImcActivity.kt
@@ -1,11 +1,47 @@
 package com.example.healthtrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var ViewVerMiImc: CardView
+    private lateinit var ViewActualizarMiImc: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+        initComponents()
+        initListeners()
+    }
+
+    private fun initComponents() {
+        ViewVerMiImc = findViewById(R.id.ViewVerTuImc)
+        ViewActualizarMiImc = findViewById(R.id.ViewCambiarMiImc)
+
+    }
+
+    private fun initListeners() {
+        ViewVerMiImc.setOnClickListener {
+            navigateToVerMiImc()
+        }
+
+        ViewActualizarMiImc.setOnClickListener {
+            navigateToActualizarMiImc()
+        }
+
+
+    }
+
+    private fun navigateToVerMiImc() {
+        val intent = Intent(this, VerMiImcActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToActualizarMiImc() {
+        val intent = Intent(this, ActualizarMiImcActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/healthtrack/RecompensasActivity.kt
+++ app/src/main/java/com/example/healthtrack/RecompensasActivity.kt
@@ -1,11 +1,53 @@
 package com.example.healthtrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 
 class RecompensasActivity : AppCompatActivity() {
+
+    private lateinit var ViewColeccion: CardView
+    private lateinit var ViewCanjear: CardView
+    private lateinit var tvPuntos: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recompensas)
+        val valorPuntos:Int = 0
+        initComponents()
+        initUI(valorPuntos)
+        initListeners()
+    }
+
+    private fun initComponents() {
+        ViewColeccion = findViewById(R.id.ViewColeccion)
+        ViewCanjear = findViewById(R.id.ViewCanjear)
+        tvPuntos = findViewById(R.id.tvPuntos)
+    }
+
+    private fun initListeners() {
+        ViewColeccion.setOnClickListener {
+            navigateColeccion()
+        }
+
+        ViewCanjear.setOnClickListener {
+            navigateCanjear()
+        }
+
+    }
+
+    private fun navigateColeccion() {
+        val intent = Intent(this, ColecccionActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateCanjear() {
+        val intent = Intent(this, CanjearActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun initUI(valorPuntos: Int){
+        tvPuntos.text = valorPuntos.toString()
     }
 }
