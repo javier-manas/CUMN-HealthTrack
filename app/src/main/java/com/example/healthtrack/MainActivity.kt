9272f638fe3363e805461f475d931fda9f782b0d
javier-manas <javier.manasb@alumnos.upm.es>
2023-04-28 04:10:03 +0200

    se incluye la feature de autenticacion y de inicio de sesion

--- app/src/main/java/com/example/healthtrack/MainActivity.kt
+++ app/src/main/java/com/example/healthtrack/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.healthtrack
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.cardview.widget.CardView
+import com.google.firebase.auth.FirebaseAuth
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
+import com.google.firebase.ktx.initialize
+import com.google.firebase.ktx.options
 
 class MainActivity : AppCompatActivity() {
 
     private var usuario: String = "True"
 
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
     private lateinit var viewIniciarSesion: CardView
+    private lateinit var etEmailAddress: TextView
+    private lateinit var etPassword: TextView
+
 
     companion object{
         const val USARIO_KEY = "True"
@@ -20,16 +34,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         initComponents()
         initListeners()
+
+
+
     }
 
     private fun initComponents() {
         viewIniciarSesion = findViewById(R.id.viewIniciarSesion)
-
+        etEmailAddress = findViewById(R.id.etEmailAddress)
+        etPassword = findViewById(R.id.etPassword)
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
     private fun initListeners() {
         viewIniciarSesion.setOnClickListener {
-            navigateToMenu(usuario)
+            signIn(etEmailAddress.text.toString(),etPassword.text.toString())
         }
 
     }
@@ -39,5 +58,21 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra(USARIO_KEY, usuario)
         startActivity(intent)
     }
+
+    private fun signIn(email: String, password: String){
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener (this)  { task ->
+                if (task.isSuccessful){
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(baseContext,"Usuario y contraseña correctos", Toast.LENGTH_SHORT).show()
+                    navigateToMenu(usuario)
+                }
+                else
+                {
+                    Toast.makeText(baseContext,"Usuario o contraseña no correcto", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 
 }
